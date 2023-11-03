4cb75c83e2f3b6735be9436abf435a86f696106c
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-03 22:18:01 +1100

    Fix HomeProfileMenu

--- frontend/src/components/HomeComponents/HomeProfileMenu.tsx
+++ frontend/src/components/HomeComponents/HomeProfileMenu.tsx
@@ -1,17 +1,18 @@
 import { Menu, Transition } from '@headlessui/react';
-import { Fragment, React } from 'react';
+import React, { Fragment } from 'react';
 import ProfileIcon from '../../assets/profileIcon.svg';
+import { HomeProfileMenuProps } from '../../types/types';
 
 export default function HomeProfileMenu ({
   openLoginModal,
   openRegisterModal,
   isLoggedIn,
   handleLogout,
-  navigateHostedListings
-}) {
+  navigateHostedListings,
+}: HomeProfileMenuProps) {
   return (
     <div>
-      <Menu as='div' className='relative inline-block text-left'>
+      <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button
             className={`w-10 justify-center rounded-full h-10 px-4 py-2 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 ${
@@ -20,29 +21,29 @@ export default function HomeProfileMenu ({
                 : 'bg-black hover:bg-blue-500'
             }`}
           >
-            <img src={ProfileIcon} alt='Profile Icon' className='w-6 h-6' />
+            <img src={ProfileIcon} alt="Profile Icon" className="w-6 h-6" />
           </Menu.Button>
         </div>
         <Transition
           as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
-            <div className='px-1 py-1 '>
-              {isLoggedIn
-                ? (
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              {isLoggedIn ? (
                 <>
                   <Menu.Item>
                     {({ active }) => (
                       <button
                         className={`${
                           active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm font-bold`} onClick={navigateHostedListings}
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm font-bold`}
+                        onClick={navigateHostedListings}
                       >
                         View Listings
                       </button>
@@ -62,8 +63,7 @@ export default function HomeProfileMenu ({
                     )}
                   </Menu.Item>
                 </>
-                  )
-                : (
+              ) : (
                 <>
                   <Menu.Item>
                     {({ active }) => (
@@ -90,7 +90,7 @@ export default function HomeProfileMenu ({
                     )}
                   </Menu.Item>
                 </>
-                  )}
+              )}
             </div>
           </Menu.Items>
         </Transition>
