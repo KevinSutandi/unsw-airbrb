0b708c131edd440d2b50de9cea37efb957f348b8
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-03 18:43:22 +1100

    Change auth.ts

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -2,6 +2,6 @@ export const getToken = () => {
   return localStorage.getItem('token');
 };
 
-export const setToken = (token) => {
+export const setToken = (token: string) => {
   return localStorage.setItem('token', token);
 };
