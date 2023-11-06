d6871fe2df0e1dc84dff4936e745b61b554b8304
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-06 17:10:30 +1100

    Add more tests

--- frontend/src/components/HomeComponents/HomeComponents.test.tsx
+++ frontend/src/components/HomeComponents/HomeComponents.test.tsx
@@ -28,4 +28,56 @@ describe('Home Components', () => {
       expect(getByRole('menuitem', { name: /Login/i })).toBeInTheDocument();
     });
   });
+
+  it('Opening sign up modal', () => {
+    const mockOpenLoginModal = jest.fn();
+    const mockOpenRegisterModal = jest.fn();
+    const mockHandleLogout = jest.fn();
+    const mockNavigateHostedListings = jest.fn();
+
+    const { getByRole } = render(
+      <HomeProfileMenu
+        openLoginModal={mockOpenLoginModal}
+        openRegisterModal={mockOpenRegisterModal}
+        isLoggedIn={false}
+        handleLogout={mockHandleLogout}
+        navigateHostedListings={mockNavigateHostedListings}
+      />
+    );
+    const profileButton = screen.getByRole('button', {
+      name: /profile icon/i,
+    });
+    fireEvent.click(profileButton);
+
+    const signUpButton = getByRole('menuitem', { name: /Sign up/i });
+
+    fireEvent.click(signUpButton);
+    expect(mockOpenRegisterModal).toHaveBeenCalled();
+  });
+
+  it('Opening log in modal', () => {
+    const mockOpenLoginModal = jest.fn();
+    const mockOpenRegisterModal = jest.fn();
+    const mockHandleLogout = jest.fn();
+    const mockNavigateHostedListings = jest.fn();
+
+    const { getByRole } = render(
+      <HomeProfileMenu
+        openLoginModal={mockOpenLoginModal}
+        openRegisterModal={mockOpenRegisterModal}
+        isLoggedIn={false}
+        handleLogout={mockHandleLogout}
+        navigateHostedListings={mockNavigateHostedListings}
+      />
+    );
+    const profileButton = screen.getByRole('button', {
+      name: /profile icon/i,
+    });
+    fireEvent.click(profileButton);
+
+    const loginButton = getByRole('menuitem', { name: /Login/i });
+
+    fireEvent.click(loginButton);
+    expect(mockOpenLoginModal).toHaveBeenCalled();
+  });
 });
