a663842ddbce2c16b58f1387c034a6f899095f89
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-06 17:17:14 +1100

    Make HomeComponentsTest more concise

--- frontend/src/components/HomeComponents/HomeComponents.test.tsx
+++ frontend/src/components/HomeComponents/HomeComponents.test.tsx
@@ -3,13 +3,13 @@ import HomeProfileMenu from './HomeProfileMenu';
 import React from 'react';
 
 describe('Home Components', () => {
-  it('Opening profile icon menu', async () => {
-    const mockOpenLoginModal = jest.fn();
-    const mockOpenRegisterModal = jest.fn();
-    const mockHandleLogout = jest.fn();
-    const mockNavigateHostedListings = jest.fn();
+  const mockOpenLoginModal = jest.fn();
+  const mockOpenRegisterModal = jest.fn();
+  const mockHandleLogout = jest.fn();
+  const mockNavigateHostedListings = jest.fn();
 
-    const { getByRole } = render(
+  const setup = () => {
+    render(
       <HomeProfileMenu
         openLoginModal={mockOpenLoginModal}
         openRegisterModal={mockOpenRegisterModal}
@@ -18,64 +18,37 @@ describe('Home Components', () => {
         navigateHostedListings={mockNavigateHostedListings}
       />
     );
-    const profileButton = screen.getByRole('button', {
-      name: /profile icon/i,
-    });
+    const profileButton = screen.getByRole('button', { name: /profile icon/i });
     fireEvent.click(profileButton);
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Opening profile icon menu', async () => {
+    setup();
 
     await waitFor(() => {
-      expect(getByRole('menuitem', { name: /Sign up/i })).toBeInTheDocument();
-      expect(getByRole('menuitem', { name: /Login/i })).toBeInTheDocument();
+      expect(
+        screen.getByRole('menuitem', { name: /Sign up/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole('menuitem', { name: /Login/i })
+      ).toBeInTheDocument();
     });
   });
 
   it('Opening sign up modal', () => {
-    const mockOpenLoginModal = jest.fn();
-    const mockOpenRegisterModal = jest.fn();
-    const mockHandleLogout = jest.fn();
-    const mockNavigateHostedListings = jest.fn();
-
-    const { getByRole } = render(
-      <HomeProfileMenu
-        openLoginModal={mockOpenLoginModal}
-        openRegisterModal={mockOpenRegisterModal}
-        isLoggedIn={false}
-        handleLogout={mockHandleLogout}
-        navigateHostedListings={mockNavigateHostedListings}
-      />
-    );
-    const profileButton = screen.getByRole('button', {
-      name: /profile icon/i,
-    });
-    fireEvent.click(profileButton);
-
-    const signUpButton = getByRole('menuitem', { name: /Sign up/i });
-
+    setup();
+    const signUpButton = screen.getByRole('menuitem', { name: /Sign up/i });
     fireEvent.click(signUpButton);
     expect(mockOpenRegisterModal).toHaveBeenCalled();
   });
 
   it('Opening log in modal', () => {
-    const mockOpenLoginModal = jest.fn();
-    const mockOpenRegisterModal = jest.fn();
-    const mockHandleLogout = jest.fn();
-    const mockNavigateHostedListings = jest.fn();
-
-    const { getByRole } = render(
-      <HomeProfileMenu
-        openLoginModal={mockOpenLoginModal}
-        openRegisterModal={mockOpenRegisterModal}
-        isLoggedIn={false}
-        handleLogout={mockHandleLogout}
-        navigateHostedListings={mockNavigateHostedListings}
-      />
-    );
-    const profileButton = screen.getByRole('button', {
-      name: /profile icon/i,
-    });
-    fireEvent.click(profileButton);
-
-    const loginButton = getByRole('menuitem', { name: /Login/i });
+    setup();
+    const loginButton = screen.getByRole('menuitem', { name: /Login/i });
 
     fireEvent.click(loginButton);
     expect(mockOpenLoginModal).toHaveBeenCalled();
