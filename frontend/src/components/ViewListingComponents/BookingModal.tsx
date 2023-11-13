109d37663b1f37be38dfbcc974353e558fb8b141
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-13 17:16:31 +1100

    Decorate

--- frontend/src/components/ViewListingComponents/BookingModal.tsx
+++ frontend/src/components/ViewListingComponents/BookingModal.tsx
@@ -11,19 +11,16 @@ export default function BookingModal ({ price }: BookingModalProps) {
       <div className="lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 box-border">
         <div className="rounded-2xl bg-gray-50 py-5 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center shadow-xl px-10">
           <div className="mx-auto">
-            <h3 className="font-bold text-left text-2xl font-semibol">
+            <h3 className="font-bold text-left text-2xl font-semibol mb-5">
               ${price} USD
             </h3>
             <DateForm />
             <a
               href="#"
-              className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="mt-10 block w-full rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Book Now
             </a>
-            <p className="mt-6 text-xs leading-5 text-gray-600">
-              Invoices and receipts available for easy company reimbursement
-            </p>
           </div>
         </div>
       </div>
--- frontend/src/components/ViewListingComponents/DateForm.tsx
+++ frontend/src/components/ViewListingComponents/DateForm.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function DateForm () {
+  const [focusedInput, setFocusedInput] = useState<string | null>(null);
+
+  const handleFocus = (inputName: 'checkin' | 'checkout') =>
+    setFocusedInput(inputName);
+  const handleBlur = () => setFocusedInput(null);
+
+  const getDivClass = (inputName: 'checkin' | 'checkout') => {
+    return `flex flex-1 py-2.5 px-2.5 rounded-md ${
+      focusedInput === inputName ? 'ring-2 ring-black cursor-text' : 'border-black cursor-pointer'
+    } `;
+  };
+
   return (
-    <div className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full flex box-border gap-1 text-left justify-between">
-      <div className="flex flex-1 py-2.5 px-2.5 border-r-2 border-black">
+    <div className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full flex box-border gap-1 text-left justify-between cursor-pointer">
+      <div className={getDivClass('checkin')}>
         <div>
-          <div>CHECK-IN</div>
+          <label htmlFor='checkin' className='cursor-pointer font-medium'>CHECK-IN</label>
           <input
             type="date"
-            className="w-full border-none h-5 p-0"
+            className="w-full border-none h-5 p-0 focus:ring-0 cursor-text bg-transparent"
             placeholder="Select date"
+            onFocus={() => handleFocus('checkin')}
+            onBlur={handleBlur}
+            id='checkin'
           />
         </div>
       </div>
-      <div className="flex flex-1 py-2.5 px-2.5">
+      <div className={getDivClass('checkout')}>
         <div>
-          <div>CHECKOUT</div>
-          <input type="date" className="w-full border-none h-5 p-0" />
+          <label htmlFor='checkout' className='cursor-pointer font-medium'>CHECKOUT</label>
+          <input
+            type="date"
+            className="w-full border-none h-5 p-0 focus:outline-none focus:ring-0 cursor-text bg-transparent"
+            onFocus={() => handleFocus('checkout')}
+            onBlur={handleBlur}
+            id='checkout'
+          />
         </div>
       </div>
     </div>
