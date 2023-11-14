dd600ccdf08137a033ee43b8109d433b39fb2063
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-14 22:58:57 +1100

    Debug

--- frontend/src/screens/HomePage.tsx
+++ frontend/src/screens/HomePage.tsx
@@ -138,8 +138,9 @@ export default function HomePage ({
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {products.length > 0
             ? (
-                products.map((product) => (
-              <NavLink to={`/listings/view/${product.id}`} key={product.id} className='group relative'>
+                products.map((product) => {
+                  console.log(product)
+                  return <NavLink to={`/listings/view/${product.id}`} key={product.id} className='group relative'>
                 <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
                   <img
                     src={product.thumbnail}
@@ -171,7 +172,7 @@ export default function HomePage ({
                   </p>
                 </div>
               </NavLink>
-                ))
+                })
               )
             : isFiltered
               ? (
