4bd2fba5118821bf11e55973625b2e3d9bc01435
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-14 19:15:05 +1100

    Add edit listing to footer

--- frontend/src/components/ViewListingComponents/BookingFooter.tsx
+++ frontend/src/components/ViewListingComponents/BookingFooter.tsx
@@ -1,19 +1,23 @@
 import React, { useContext } from 'react';
 import DateContext from './DateContext';
 import { Nullable } from 'primereact/ts-helpers';
+import { getEmail } from '../../utils/auth';
+import { NavLink } from 'react-router-dom';
 
 type BookingModalProps = {
   price: number;
   openDateModal: () => void;
-  owner: string
+  owner: string;
   handleBook: () => void;
-  listingId: string
+  listingId: string;
 };
 
 export default function BookingFooter ({
   price,
   openDateModal,
-  handleBook
+  handleBook,
+  owner,
+  listingId,
 }: BookingModalProps) {
   const contextValue = useContext(DateContext);
 
@@ -38,7 +42,10 @@ export default function BookingFooter ({
   return (
     <footer className="w-full border-t border-black xl:hidden flex justify-between p-5 items-center sticky bottom-0 bg-white left-0">
       <div>
-        <div className="font-bold text-2xl">${price} AUD</div>
+        <div className='flex items-baseline gap-2'>
+          <div className="font-bold text-2xl">${price} AUD</div>
+          <p>Per Night</p>
+        </div>
         <button
           className="underline text-lg font-semibold"
           onClick={openDateModal}
@@ -46,11 +53,25 @@ export default function BookingFooter ({
           {formatDate(checkinDate)} - {formatDate(checkoutDate)}
         </button>
       </div>
-      <button
-      onClick={handleBook}
-      className="inline-block rounded-md bg-blue-600 px-8 py-4 text-center text-2xl font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      {owner === getEmail()
+        ? (
+        <NavLink to={`/listings/edit/${listingId}`} className="inline-block rounded-md bg-blue-600 px-8 py-4 text-center text-2xl font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >Edit Listing</NavLink>
+          )
+        : (
+        <button
+          onClick={handleBook}
+          className="inline-block rounded-md bg-blue-600 px-8 py-4 text-center text-2xl font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Book Now
+        </button>
+          )}
+      {/* <button
+        onClick={handleBook}
+        className="inline-block rounded-md bg-blue-600 px-8 py-4 text-center text-2xl font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
         Book Now
-      </button>
+      </button> */}
     </footer>
   );
 }
