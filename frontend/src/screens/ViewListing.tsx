b9cdfd2b0696269212c0536d45da393c54521036
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-15 13:31:17 +1100

    Add todos

--- frontend/src/screens/ViewListing.tsx
+++ frontend/src/screens/ViewListing.tsx
@@ -16,6 +16,9 @@ import BookConfirmation from '../components/ViewListingComponents/BookConfirmati
 import AmenitiesList from '../components/ViewListingComponents/AmenitiesList';
 import GlobalContext from '../components/GlobalContext';
 
+// TODO: Adjust reviews accordingly
+// TODO: Calendar to date earlier than from date validation
+
 export const calculateDifferenceInDays = (
   date1: Nullable<Date>,
   date2: Nullable<Date>
